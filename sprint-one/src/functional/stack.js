51c4531fc539abad31513ad98aef8880901b0eb5
Mandy Trinh <saphiric@gmail.com>
2019-01-04 12:30:41 -0800

    completed stack implemention in functional pattern

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,34 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  someInstance.storage = storage;
+  someInstance.count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    someInstance.storage[someInstance.count] = value;
+    someInstance.count += 1;
+    return someInstance.storage;
   };
 
   someInstance.pop = function() {
+    if (someInstance.count === 0) {
+      return 0;
+    }
+    var idxToRemove = someInstance.count - 1;
+    // console.log(idxToRemove);
+    var itemToRemove = someInstance.storage[idxToRemove];
+    delete someInstance.storage[idxToRemove];
+    someInstance.count -= 1;
+    return itemToRemove;
+    // return someInstance.storage;
   };
 
   someInstance.size = function() {
+    if (someInstance.storage === {}) {
+      return 0;
+    }
+    return someInstance.count;
   };
-
   return someInstance;
 };
