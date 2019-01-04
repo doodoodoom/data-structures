17f70651f615213244d12777390560591984fdce
Mandy Trinh <saphiric@gmail.com>
2019-01-04 15:52:29 -0800

    completed pseudoclassical pattern for stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(Stack.stackMethods);
+  someInstance.storage = {};
+  someInstance.count = 0;
+  return someInstance;
 };
 
-var stackMethods = {};
+Stack.stackMethods = {};
 
+Stack.stackMethods.push = function(value) {
+  this.storage[this.count] = value; 
+  //e.g {0: "first val", 1: "second val"}
+  this.count += 1;
+  return this.storage;
+};
 
+// the last elem is the size minus 1
+Stack.stackMethods.pop = function() {
+  if (this.count === 0) {
+    return 0;
+  }
+  var lastIdx = this.count - 1;
+  var elemToRemove = this.storage[lastIdx];
+  delete this.storage[lastIdx];
+  this.count -= 1;
+  return elemToRemove;
+};
+
+Stack.stackMethods.size = function() {
+  return this.count;
+};
+var s = Stack();
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,30 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.count = 0;
 };
 
+// Stack.stackMethods = {};
 
+Stack.prototype.push = function(value) {
+  this.storage[this.count] = value; 
+  //e.g {0: "first val", 1: "second val"}
+  this.count += 1;
+  return this.storage;
+};
+
+// the last elem is the size minus 1
+Stack.prototype.pop = function() {
+  if (this.count === 0) {
+    return 0;
+  }
+  var lastIdx = this.count - 1;
+  var elemToRemove = this.storage[lastIdx];
+  delete this.storage[lastIdx];
+  this.count -= 1;
+  return elemToRemove;
+};
+
+Stack.prototype.size = function() {
+  return this.count;
+};
+var s = new Stack();
