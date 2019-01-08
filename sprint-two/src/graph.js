63f4969fdff0dde4c6b1a2ca08632da4b0e79142
doodoodoom <madridmatt17@gmail.com>
2019-01-08 13:56:52 -0800

    completed graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,6 +1,4 @@
 var Graph = function() {
-  // this = {};
-  //this.value = value;
   this.counter = 0;
 };
 
@@ -36,11 +34,8 @@ basic case: { 0: {value: 4, edges: [] } } --> remove 4 --> { }
 edge case: {0: {value: 4, edges: [5, 3]}, 1: {value: 5, edges: [4, 3] }, 2: {value: 3, edges [4, 5]} }
 --> remove 4 
 --> {1: {value: 5, edges: [3] }, 2: {value: 3, edges [5]} }
-
 */
 Graph.prototype.removeNode = function(node) {
-  // loop thru graph obj based on counter (key)
-  // for each node object, if the obj's value === node, delete the graph obj's key
   var isDeleted = false;
   for (var count in this) {
     if (typeof this[count] === "object") {
@@ -69,8 +64,8 @@ Graph.prototype.removeNode = function(node) {
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 /*
 {0: {value: 4, edges: [5, 3]}, 1: {value: 5, edges: [4, 3] }, 2: {value: 3, edges [4, 5]} }
-g.hasEdge(4, 3) will check for fromNode, and see if its edges property includes toNode
-will check for toNode, and see if its edges property includes fromNode
+g.hasEdge(4, 3) first checks for fromNode, and see if its edges property includes toNode
+Then checks for toNode, and see if its edges property includes fromNode
  */
 Graph.prototype.hasEdge = function(fromNode, toNode) {
   var hasFromNode = false;
@@ -95,13 +90,16 @@ Graph.prototype.hasEdge = function(fromNode, toNode) {
   return hasFromNode && hasToNode;
 };
 
-// Connects two nodes in a graph by adding an edge between them.
-// { 0: {value: 4, edges: [] }, 1: {value: 5, edges: [] } } --> addEdge(4, 5)
-// --> {0: {value: 4, edges: [5]}, 1: {value: 5, edges: [4] } }
+/*
+Connects two nodes in a graph by adding an edge between them.
+{ 0: {value: 4, edges: [] }, 1: {value: 5, edges: [] } } --> addEdge(4, 5)
+--> {0: {value: 4, edges: [5]}, 1: {value: 5, edges: [4] } }
+  loop thru graph obj
+  if fromNode === value, then add toNode to fromNode's edges, 
+  if toNode === value, then add fromNode to toNode's edges
+*/
+
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  //loop thru graph obj, find fromNode || toNode 
-  // if fromNode === value, then add toNode to fromNode's edges, 
-  // if toNode === value, then add fromNode to toNode's edges
   for (var count in this) {
     var singleNode = this[count];
     if (singleNode.value === fromNode) {
@@ -115,8 +113,34 @@ Graph.prototype.addEdge = function(fromNode, toNode) {
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  for (var count in this) {
+    if (typeof this[count] === "object") {
+      var singleNode = this[count];
+      if (singleNode.value === fromNode) {
+        for (var i = 0; i < singleNode.edges.length; i++) {
+          if (singleNode.edges[i] === toNode) {
+            singleNode.edges.splice(i, 1);
+          }
+        }
+      }
+      if (singleNode.value === toNode) {
+        for (var i = 0; i < singleNode.edges.length; i++) {
+          if (singleNode.edges[i] === fromNode) {
+            singleNode.edges.splice(i, 1);
+          }
+        }
+      }
+    }
+  }
+  
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (var count in this) {
+    if (typeof this[count] === "object") {
+      var singleNode = this[count];
+      cb(singleNode.value);
+    }
+  }
 };
