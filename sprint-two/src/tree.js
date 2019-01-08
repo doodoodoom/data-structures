1d014486df6cf466f7e5648b1e9c6d5a5f96cec5
doodoodoom <madridmatt17@gmail.com>
2019-01-07 17:38:32 -0800

    finished basic tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,21 +2,42 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  //.children property, an array containing a number of subtrees
+  // [{[ {}, {}], value: ___ }, {}, {}]
+  newTree.children = [];  
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
 
   return newTree;
 };
 
 var treeMethods = {};
 
+// takes any value, sets that as the target of a node, and adds that node as a child of the tree
+// 
 treeMethods.addChild = function(value) {
+  var subTree = Tree(value);
+  this.children.push(subTree);
+  return this;
 };
 
 treeMethods.contains = function(target) {
+    var wasFound = false;
+    function helper(childTree) {
+      if (childTree.value === target) {
+          wasFound = true;
+          return;
+      }
+      if (childTree.children.length) {
+          for (var i = 0; i < childTree.children.length; i++) {
+//           console.log("current value is ", childTree.children[i].value);
+          helper(childTree.children[i]);
+        }
+      }
+   }
+    helper(this);
+    return wasFound;
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
